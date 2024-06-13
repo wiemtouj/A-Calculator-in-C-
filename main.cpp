dc200d27793773cb39ad728dd51a39010ceb1a6c
wiemtouj <toujwiem@gmail.com>
2024-06-13 13:49:42 +0100

    update

--- main.cpp
+++ main.cpp
@@ -1,9 +1,46 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello driis!" << endl;
-    return 0
+
+
+   double number1,number2;
+   string operation;
+
+   cout << "Enter first number: ";
+
+   cin >> number1;
+
+   cout << "Enter operation (+, -, *, /): ";
+
+   cin >> operation;
+
+   cout << "Enter second number: ";
+
+   cin >> number2;
+
+   if (operation == "+"){
+        cout << number1 + number2;
+   }
+
+   else if (operation == "-"){
+        cout << number1 - number2;
+   }
+
+   else if (operation == "*"){
+        cout << number1 * number2;
+   }
+
+   else if (operation == "/"){
+        cout << number1 / number2;
+   }
+
+   else {cout << "Not a recognize operation!";
+   }
+
+
+    return 0;
 }
